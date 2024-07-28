e17c83d10e704a193be083a33620633ff1b146b0
shady <shadyngheith@gmail.com>
2024-07-29 00:12:20 +0300

    Change the token expiration date

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -7,7 +7,7 @@ const generateToken = (user) => {
         throw new Error('Error in generation token')
     }
     const token = jwt.sign({ id: user._id }, process.env.PRIVATE_KEY, {
-        expiresIn: '1w',
+        expiresIn: '7d',
     });
 
     return token
